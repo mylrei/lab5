fb41107d5b48acd916a6b65d4dae22c7187e0147
Riley McDermott <rileymcdermott@college.harvard.edu>
2019-02-24 09:47:46 -0800

    final final

--- lab5.ml
+++ lab5.ml
@@ -4,18 +4,7 @@
  *)
 
 (*
-Objective:
-
-In this lab you'll practice concepts of algebraic data types,
-including product types (like tuples) and sum types (like variants),
-and the expressive power that arises from combining both. A theme will
-be the requirement of and checking for consistency with invariants.
-
-NOTE: Since we ask that you define types in this lab, you must
-complete certain exercises before this will compile with the testing
-framework on the course grading server. Exercises 1, 2, 6, and 9 are
-required for full compilation.
-
+                               SOLUTION
  *)
 
 (*==============================================================================
@@ -56,7 +45,15 @@ be any of the following options: red, crimson, orange, yellow, green,
 blue, indigo, or violet.
 ......................................................................*)
 
-type color_label = Red | Crimson | Orange | Yellow | Green | Blue | Indigo | Violet;;
+type color_label =
+  | Red
+  | Crimson
+  | Orange
+  | Yellow
+  | Green
+  | Blue
+  | Indigo
+  | Violet ;;
 
 (* But this is an overly simple representation of colors. Let's make
 it more usable.
@@ -92,7 +89,9 @@ channels. You'll want to use Simple and RGB as the value constructors
 in this new variant type.
 ......................................................................*)
 
-type color =  Simple of color_label | RGB of int*int*int;;
+type color =
+  | Simple of color_label
+  | RGB of int * int * int ;;
 
 (* There is an important assumption about the RGB values that
 determine whether a color is valid or not. The RGB type presupposes an
@@ -139,11 +138,15 @@ an Invalid_color exception with a useful message.
 
 exception Invalid_color of string ;;
 
-let validated_rgb (c :color) : color =
-   match c with
-   |Simple a -> Simple a
-   | RGB (x,y,z) -> if x>=0 && x<=255 && y>=0 && y<=255 && z>=0 && z<=255 then RGB (x,y,z) else
-   raise (Invalid_color "not a valid color");;
+let validated_rgb (c : color) : color =
+  let bad (x : int) : bool = (x < 0 || x > 255) in
+  match c with
+  | Simple x -> c
+  | RGB (r, g, b) ->
+     if bad r then raise (Invalid_color "bad red channel")
+     else if bad g then raise (Invalid_color "bad green channel")
+     else if bad b then raise (Invalid_color "bad blue channel")
+     else c ;;
 
 (*......................................................................
 Exercise 4: Write a function, make_color, that accepts three integers
@@ -151,8 +154,8 @@ for the channel values and returns a value of the color type. Be sure
 to verify the invariant.
 ......................................................................*)
 
-let make_color (r: int) (g : int) (b: int) : color =
-  validated_rgb (RGB (r,g,b));;
+let make_color (r : int) (g : int) (b : int) : color =
+  validated_rgb (RGB (r, g, b)) ;;
 
 (*......................................................................
 Exercise 5: Write a function, convert_to_rgb, that accepts a color and
@@ -169,17 +172,19 @@ below are some other values you might find helpful.
     240 | 130 | 240 | Violet
 ......................................................................*)
 
-let convert_to_rgb (c: color) : int*int*int =
+let convert_to_rgb (c : color) : int * int * int =
   match c with
-  |Simple Red -> (255,0,0)
-  |Simple Green -> (0,255,0)
-  |Simple Blue -> (0,0,255)
-  |Simple Orange -> (255,165,0)
-  |Simple Yellow -> (255,255,0)
-  |Simple Indigo -> (75,0,130)
-  |Simple Violet -> (240,130,240)
-  |Simple Crimson -> (164,16,52)
-  |RGB (r,g,b)-> if validate_rgb (RGB(r,g,b)) == RGB(r,g,b) then (r,g,b);;
+  | RGB (r, g, b) -> (r, g, b)
+  | Simple x ->
+     match x with
+     | Red ->     (255,   0,   0)
+     | Crimson -> (164,  16,  52)
+     | Orange ->  (255, 165,   0)
+     | Yellow ->  (255, 255,   0)
+     | Green ->   (  0, 255,   0)
+     | Blue ->    (  0,   0, 255)
+     | Indigo ->  ( 75,   0, 130)
+     | Violet ->  (240, 130, 240) ;;
 
 (*======================================================================
 Part 2: Dates as a record type
@@ -204,7 +209,7 @@ should be. Then, consider the implications of representing the overall
 data type as a tuple or a record.
 ......................................................................*)
 
-type date = NotImplemented ;;
+type date = { year : int; month : int; day : int } ;;
 
 (* After you've thought it through, look up the Date module in the
 OCaml documentation to see how this was implemented there. If you
@@ -246,8 +251,34 @@ the invariant is violated, and returns the date if valid.
 
 exception Invalid_date of string ;;
 
-let validated_date =
-  fun _ -> failwith "validated_date not implemented" ;;
+let validated_date ({year; month; day} as date) : date =
+  if year < 0 then raise (Invalid_date "negative year")
+  else
+    let leap = (year mod 4 = 0 && year mod 100 <> 0)
+               || year mod 400 = 0 in
+    let max_days =
+      match month with
+      | 1 | 3 | 5 | 7 | 8 | 10 | 12 -> 31
+      | 4 | 6 | 9 | 11 -> 30
+      | 2 -> if leap then 29 else 28
+      | _ -> raise (Invalid_date "bad month") in
+    if day > max_days then raise (Invalid_date "day too large")
+    else if day < 1 then raise (Invalid_date "day too small")
+    else date ;;
+
+(* This code makes use of a useful previously unseen construct of
+OCaml's matching, which allows the naming of the parts of a data item
+and the whole item as well. In the pattern "{year; month; day} as date",
+the result of the pattern is to name the whole record d while still
+allowing for accessing the parts with names year, month, and day.
+
+An issue to consider is the priority of the various invariants. It
+makes sense to check the validity of the year and the month before
+moving on to check the validity of the day, as we've done here, since
+the validity of the day relies on the validity of the month and
+year. That is, in case the year or month is invalid, we'd rather raise
+an exception complaining about that than one complaining about an
+invalid day. *)
 
 (*======================================================================
 Part 3: Family trees as an algebraic data type
@@ -260,7 +291,7 @@ Exercise 9: Define a person record type. Use the field names "name",
 "favorite", and "birthdate".
 ......................................................................*)
 
-type person = NotImplemented ;;
+type person = { name : string; favorite : color; birthdate : date } ;;
 
 (* Let's now do something with these person values. We'll create a
 data structure that allows us to model simple familial relationships.
@@ -299,8 +330,10 @@ ensure the invariants are preserved for color and date, use them here
 as well.
 ......................................................................*)
 
-let new_child =
-  fun _ -> failwith "new_child not implemented" ;;
+let new_child (name : string) (col : color) (birth : date) : family =
+  Single { name;
+           favorite = validated_rgb col;
+           birthdate = validated_date birth } ;;
 
 (*......................................................................
 Exercise 11: Write a function that allows a person to marry in to a
@@ -311,8 +344,11 @@ is already made up of a married couple?
 
 exception Family_Trouble of string ;;
 
-let marry =
-  fun _ -> failwith "marry not implemented" ;;
+let marry (fam : family) (spouse : person) : family =
+  match fam with
+  | Single per -> Family (per, spouse, [])
+  | Family _ -> raise (Family_Trouble ("cannot add "
+                                       ^ spouse.name ^ " to a couple")) ;;
 
 (*......................................................................
 Exercise 12: Write a function that accepts two families, and returns
@@ -323,13 +359,18 @@ assumptions provided in the type definition of family to determine how
 to behave in corner cases.
 ......................................................................*)
 
-let add_to_family =
-  fun _ -> failwith "add_to_family not implemented" ;;
+let add_to_family (fam : family) (child : family) : family =
+  match fam with
+  | Single _ -> raise (Family_Trouble "singles don't have children")
+  | Family (p1, p2, children) -> Family (p1, p2, child :: children) ;;
 
 (*......................................................................
 Exercise 13: Complete the function below that counts the number of
 people in a given family. Be sure you count all spouses and children.
 ......................................................................*)
 
-let count_people =
-  fun _ -> failwith "count_people not implemented" ;;
+let rec count_people (fam: family) : int =
+  match fam with
+  | Single _ -> 1
+  | Family (_, _, c) -> 2 + List.fold_left (+) 0
+                                           (List.map count_people c) ;;
